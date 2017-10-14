c5deaae7a246796a856589454dcd8ccbaff0d074
Daniel Koza <dakolech@gmail.com>
2017-10-14 18:24:08 +0200

    add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './App.css';
 import { CounterComponent } from './counter/components/counter.component';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 class App extends React.Component {
   render() {
